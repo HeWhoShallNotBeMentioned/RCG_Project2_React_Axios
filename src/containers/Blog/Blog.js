be5d6a554e54ef7ce8e8c0f7289db013fdd8c08c
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-03 17:38:43 -0700

    made it possible to select a post. Data is not yet passed.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,7 @@ import './Blog.css';
 class Blog extends Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = { posts: [], selectedPostId: null };
   }
 
   async componentDidMount() {
@@ -28,15 +28,26 @@ class Blog extends Component {
     console.log('data', data);
   }
 
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
