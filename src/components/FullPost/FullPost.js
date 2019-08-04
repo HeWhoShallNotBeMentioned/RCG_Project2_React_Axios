56350239bddb0693b49e969c4de82ddb6639f9ee
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-03 19:42:22 -0700

    set up path for deleted record

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -24,6 +24,14 @@ class FullPost extends Component {
       }
     }
   }
+
+  deletePostHandler = async () => {
+    const deletedResponse = await axios.delete(
+      'https://jsonplaceholder.typicode.com/posts/' + this.props.id
+    );
+    console.log('deletedResponse  ------------', deletedResponse);
+  };
+
   render() {
     console.log('state', this.state);
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
@@ -37,7 +45,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}t</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
