10fb766540309ba825d7cafb6509b42a24421880
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-03 19:12:19 -0700

    add code to have the post data fill the fullpost component and account for the axios query not running yet.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadedPost: null,
+    };
+  }
+  async componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        const { data } = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts/' + this.props.id
+        );
+        this.setState({ loadedPost: data });
+        console.log('post+++++++++++++++++', data);
+      }
+    }
+  }
   render() {
+    console.log('state', this.state);
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
     if (this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading.......</p>;
+    }
+
+    if (this.props.id && this.state.loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}t</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
