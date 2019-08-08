73d15de24d0e2ccaf6f096c45f945b066ea4156f
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-08 16:08:24 -0700

    added npm packages for react-router-dom. Moved files around in anticpation of setting up the app for routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -54,7 +51,7 @@ class Blog extends Component {
       <div className="Blog">
         <header>
           <nav>
-            <ul >
+            <ul>
               <li>
                 <a href="/">Home</a>
               </li>
