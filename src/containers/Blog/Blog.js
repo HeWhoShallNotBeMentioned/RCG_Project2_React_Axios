a5d785703028ae6fb07add619f23de544a608da6
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-09 13:07:54 -0700

    add a new route for new-posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 import { Route } from 'react-router-dom';
 
@@ -27,6 +28,7 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <Posts />} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
