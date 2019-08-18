fa7d59a467eb31f0bf1d0e0fd8cb2b3a53a29497
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-18 11:57:54 -0700

    add redirect from / to /posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -37,6 +37,8 @@ class Blog extends Component {
           {/* <Route path="/" exact render={() => <Posts />} /> */}
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+          {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
