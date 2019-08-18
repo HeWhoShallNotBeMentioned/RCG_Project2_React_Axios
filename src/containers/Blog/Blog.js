795478cd163a8ee0760efd441f5177ea1fc4a345
Chris Underwood <cunderwoodmn@gmail.com>
2019-08-17 20:39:19 -0700

    add switch to routes in blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -34,22 +34,15 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        {/* <Route path="/" exact render={() => <Posts />} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" exact component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          {/* <Route path="/" exact render={() => <Posts />} /> */}
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
 }
 
 export default Blog;
-
-{
-  /* <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section> */
-}
